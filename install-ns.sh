c478166d3cf20e0aed7a0d837560b8e339ac5a12
Gustaf Neumann <neumann@wu-wien.ac.at>
2024-04-15 10:05:47 +0200

    added error code in case curl fails

--- install-ns.sh
+++ install-ns.sh
@@ -651,6 +651,7 @@ chksum_get_value() {
 
 chksum_set_value tcl8.6.13-src.tar.gz      43a1fae7412f61ff11de2cfd05d28cfc3a73762f354a417c62370a54e2caf066
 chksum_set_value tcl8.6.14-src.tar.gz      5880225babf7954c58d4fb0f5cf6279104ce1cd6aa9b71e9a6322540e1c4de66
+chksum_set_value tcl-core-8-6-14.tar.gz    4a8834f8b7ec68087e21a05779758956d559c88491cc43020d445ff3edaabaab
 chksum_set_value tcl-core-8-6-13.tar.gz    69d4b1192a3ad94c1748e1802c5cf727b2dbba400f5560407f9af19f3d8fd6b3
 chksum_set_value tcl-core-8-7-a5.tar.gz    7dd250dc6a76af47f3fc96b218906cfd166edf63c5d142186d632b500a6030eb
 
@@ -680,7 +681,7 @@ function retry {
     "$@" && break || {
       if [[ $n -lt $max ]]; then
         ((n++))
-        echo "Command failed. Attempt $n/$max:"
+        echo "Command failed. Attempt $n/$max, error code: $?"
         sleep $delay;
       else
         fail "The command has failed after $n attempts."
