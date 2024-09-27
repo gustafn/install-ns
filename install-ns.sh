61dc7647e70619da9b86b5d929009e26cd8e762a
Gustaf Neumann <neumann@wu-wien.ac.at>
2024-09-27 09:38:18 +0200

    added support for tcl9.0.0

--- install-ns.sh
+++ install-ns.sh
@@ -144,6 +144,9 @@ if [ "${version_thread}" = "" ] && [ ${tcl_fetch_from_core} = "1" ] ; then
         version_thread=main
     elif [[ ${version_tcl} == *"8-5"* ]] ; then
         version_thread=thread-2-6
+    elif [[ ${version_tcl} == *"9.0.0"* ]] ; then
+        version_thread=thread-3-0-0
+        #version_thread=3.0b1
     else
         #version_thread=thread-2-8-branch
         version_thread=thread-2-8-9
@@ -670,6 +673,7 @@ chksum_set_value tcl-core-8-5-19.tar.gz    45bf6624144d063e12dcc840a27d9edfedf9a
 chksum_set_value tcl8.6.13-src.tar.gz      43a1fae7412f61ff11de2cfd05d28cfc3a73762f354a417c62370a54e2caf066
 chksum_set_value tcl8.6.14-src.tar.gz      5880225babf7954c58d4fb0f5cf6279104ce1cd6aa9b71e9a6322540e1c4de66
 chksum_set_value tcl8.6.15-src.tar.gz      861e159753f2e2fbd6ec1484103715b0be56be3357522b858d3cbb5f893ffef1
+chksum_set_value tcl9.0.0-src.tar.gz       3bfda6dbaee8e9b1eeacc1511b4e18a07a91dff82d9954cdb9c729d8bca4bbb7
 chksum_set_value tcl-core-8-6-14.tar.gz    4a8834f8b7ec68087e21a05779758956d559c88491cc43020d445ff3edaabaab
 chksum_set_value tcl-core-8-6-13.tar.gz    69d4b1192a3ad94c1748e1802c5cf727b2dbba400f5560407f9af19f3d8fd6b3
 chksum_set_value tcl-core-8-7-a5.tar.gz    7dd250dc6a76af47f3fc96b218906cfd166edf63c5d142186d632b500a6030eb
