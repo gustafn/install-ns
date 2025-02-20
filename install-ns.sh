ea431caf04b03f0e5c1f43f97ff426d4b054ed83
Gustaf Neumann <neumann@wu-wien.ac.at>
2025-02-20 10:46:09 +0100

    use "download_file" also for tclconfig

--- install-ns.sh
+++ install-ns.sh
@@ -1376,7 +1376,8 @@ else
     if [ ! -f ${thread_src_dir}/tclconfig ] ; then
         echo "Downloading tclconfig"
         url=https://core.tcl-lang.org/tclconfig/tarball/tclconfig.tar.gz?uuid=tcl8-compat
-        curl -L -s -k -o tclconfig.tar.gz $url
+        #curl -L -s -k -o tclconfig.tar.gz $url
+        download_file tclconfig.tar.gz $$url
         tar xf tclconfig.tar.gz
         ln -s ${build_dir}/tclconfig ${thread_src_dir}/tclconfig
     else
