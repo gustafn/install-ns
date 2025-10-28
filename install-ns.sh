63ac0ba808ecd3452dc0b8290d7b07b4c6e8918d
Gustaf Neumann <neumann@wu-wien.ac.at>
2025-10-28 09:41:25 +0100

    added pkg-config for debian family for "slim" installations

--- install-ns.sh
+++ install-ns.sh
@@ -624,7 +624,7 @@ with_openssl_configure_flag=
 if [ $debian = "1" ] ; then
     # On Debian/Ubuntu, make sure we have zlib installed, otherwise
     # NaviServer can't provide compression support
-    apt-get install -y make ${autoconf} locales gcc zlib1g-dev \
+    apt-get install -y make ${autoconf} pkg-config locales gcc zlib1g-dev \
             curl zip unzip openssl libssl-dev \
             ${pg_packages} ${git} ${mongodb}
     locale-gen en_US.UTF-8
@@ -774,6 +774,8 @@ chksum_set_value nsf2.4.0.tar.gz           51bd956d8db19f9bc014bec0909f73659431c
 
 #chksum_set_value thread-thread-2-8-branch.tar.gz 1674cd723f175afc55912694b01d1918539eefc3d2e8fef0b8b509f7ae77d490
 chksum_set_value thread-thread-2-8-branch.tar.gz 21d69cfb8a010957398ee6fd41a03a770941803971ae0b5d17229684cec6ce88
+
+chksum_set_value naviserver-5.0.3.tar.gz   e29ca00d6c5d3c19e38ba63305ab89d71253405f11807cdb43c4b7fa3a9db3e1
 
 # Get and print a value
 # echo "The value of key1 is: $(chksum_get_value "tdom-0.9.1-src.tgz")"
